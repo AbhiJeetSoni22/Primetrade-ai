0a360217bb31046acab4daaa87f7ccde006cdb6b
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-03-15 12:51:17 +0530

    cors issue solution

--- app/main.py
+++ app/main.py
@@ -2,9 +2,22 @@ from fastapi import FastAPI, Depends
 from app.routes.auth_routes import router as auth_router
 from app.middleware.auth_middleware import get_current_user
 from app.routes.task_routes import router as task_router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="Backend Developer Assignment API",
     version="1.0"
+)
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
