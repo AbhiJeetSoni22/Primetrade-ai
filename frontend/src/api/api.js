b6e2c41fd5f0ddbb6d7c2d6ad0511349b35675f4
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-03-15 13:01:24 +0530

    api file updated

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const backendUrl = import.meta.env.VITE_BACKEND_URL
 const API = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: backendUrl
 });
 
 export const registerUser = (data) =>
