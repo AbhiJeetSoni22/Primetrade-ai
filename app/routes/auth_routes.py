adf4757d5a08b17309f19963ad8558c580310a57
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-03-16 19:37:39 +0530

    auth route updated

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -4,12 +4,12 @@ from app.controllers.auth_controller import register_user, login_user
 
 router = APIRouter(prefix="/api/v1/auth", tags=["Authentication"])
 
-
+# route to register the user
 @router.post("/register")
 def register(user: UserRegister):
     return register_user(user)
 
-
+# route to login the user
 @router.post("/login")
 def login(user: UserLogin):
     return login_user(user)
